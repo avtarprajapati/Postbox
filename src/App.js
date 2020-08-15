3cfac762134904c7fa43a5f6eafd4366cb49f74c
karandas777 <karandas.21.08.98@gmail.com>
2020-08-15 12:03:06 +0530

    added fontface & favicon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./SCSS/main.scss";
 function App() {
   return (
     <div className="container">
+      <div className="title">Instagram</div>
       React Starter Setup with Redux and React Router
     </div>
   );
