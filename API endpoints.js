b9ba6197e04caae3550d4ad48bd67153b7d54e80
karandas777 <karandas.21.08.98@gmail.com>
2020-08-15 10:00:28 +0530

    added API endpoints file

---
+++ API endpoints.js
@@ -0,0 +1,12 @@
+
+API = "https://postboxserver.herokuapp.com/api";
+
+// user endpoints
+
+"/add-user" (post) = "Inserting a new user"
+
+"/select-user" (get) = "For getting a list of all users"
+
+"/edit-user" (post) = "Updating an existing user"
+
+"/remove-user" (post) = "Deleting an user"
