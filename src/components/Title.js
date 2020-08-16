a82f66f77cf5e7db9f37380c6ea5346d10df98bf
unknown <prajapatiavtar@gmail.com>
2020-08-16 10:14:36 +0530

    routes

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Title extends Component {
+  render() {
+    return (
+      <div className="title">
+        <div className="logo">Postbox</div>
+      </div>
+    );
+  }
+}
+
+export default Title;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Explore from "./Explore";
+import Login from "./Login";
+import Register from "./Register";
+import Profile from "./Profile";
+
+export { Home, Explore, Login, Register, Profile };
