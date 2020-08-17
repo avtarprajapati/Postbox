6697145a4fa8fbcbc1efef798b21d15cac1adbbb
unknown <prajapatiavtar@gmail.com>
2020-08-17 16:19:42 +0530

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Title extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="title">
+          <div className="logo">Postbox</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Title;
