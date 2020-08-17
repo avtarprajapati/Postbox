ca13350722f96416d508b1bd64c3ec14fb446b6d
unknown <prajapatiavtar@gmail.com>
2020-08-17 15:47:29 +0530

    add redux thunk

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import promise from "redux-promise";
-// import reduxThunk from "redux-thunk";
+import reduxThunk from "redux-thunk";
 
 import App from "./App";
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(promise)));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
