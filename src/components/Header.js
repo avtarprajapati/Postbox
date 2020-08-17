bc8ef1b896e03ef2bb16e331dfbf24399bf9cbc9
unknown <prajapatiavtar@gmail.com>
2020-08-17 17:05:44 +0530

    add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,62 @@ import React, { Component } from "react";
 export class Title extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="title">
+      <div className="container-fluid mt-2">
+        {/* <div className="title">
           <div className="logo">Postbox</div>
-        </div>
+        </div> */}
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            Postbox
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav my-auto">
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  User Names
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">
+                    Profile
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Log Out
+                  </a>
+                </div>
+              </li>
+            </ul>
+
+            <form class="form-inline my-2 my-lg-0">
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+        </nav>
       </div>
     );
   }
