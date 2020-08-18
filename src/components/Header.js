7d209300602f54dabf410a8519e41786e338a218
unknown <prajapatiavtar@gmail.com>
2020-08-18 16:22:50 +0530

    minor change

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export class Title extends Component {
     console.log(this.props.currentUser);
     const { currentUser } = this.props;
     if (!currentUser) return "please wait";
-    const imgurl = "https://avatars.dicebear.com/api/bottts/7895.svg";
+
     return (
       <div className="container-fluid p-0 bg-light shadow-sm">
         <div className="container">
@@ -24,53 +24,49 @@ export class Title extends Component {
               </Link>
               <button
                 className="btn btn-light"
-                data-toggle="collapse" data-target="#collapseExample"
+                data-toggle="collapse"
+                data-target="#collapseExample"
               >
                 <i className="fa fa-bars"></i>
               </button>
             </nav>
 
-            <div class="collapse" id="collapseExample">
+            <div className="collapse" id="collapseExample">
               <div className="p-2 row text-center">
                 <div className="col-md-3 m-0">
                   <img
-                    src={imgurl}
+                    src={currentUser.imgurl}
                     className="w-50 bg-grad-2 p-1 rounded-pill shadow my-4"
                     alt="Profile"
                   />
                   <div className="h3 mt-0 mb-4">
-                    {/* {currentUser.name} */}User
+                    {currentUser.name}
                     <i className="fa fa-check-circle text-primary ml-1"></i>
                   </div>
                   {/* <hr className="my-3 bg-grad-2 pt-1 rounded" /> */}
                 </div>
                 <div className="col-md-6"></div>
                 <div className="col-md-3 m-0">
-                  <Link
-                    to="/profile"
-                    className="btn btn-light w-100 mb-2"                  
-                  >
+                  <Link to="/profile" className="btn btn-light w-100 mb-2">
                     Profile
                   </Link>
-                  <Link
-                    to="/upload"
-                    className="btn btn-light w-100 mb-2"
-                  >
+                  <Link to="/upload" className="btn btn-light w-100 mb-2">
                     Upload
                   </Link>
-                  <Link
-                    to="/explore"
-                    className="btn btn-light w-100 mb-2"                    
-                  >
+                  <Link to="/explore" className="btn btn-light w-100 mb-2">
                     Explore
                   </Link>
                   <button
-                    className="btn btn-light text-danger w-100 mb-2"               
+                    className="btn btn-light text-danger w-100 mb-2"
                     onClick={this.onLogOut}
                   >
                     Logout
                   </button>
-                  <button className="btn btn-light" data-toggle="collapse" data-target="#collapseExample">
+                  <button
+                    className="btn btn-light"
+                    data-toggle="collapse"
+                    data-target="#collapseExample"
+                  >
                     <i className="fa fa-times"></i>
                   </button>
                 </div>
@@ -88,7 +84,7 @@ function mapStateToProps(state) {
   const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
 
   return {
-    currentUser: state.users[currentUser.userId],
+    currentUser: state.users[currentUser.userId]
   };
 }
 
