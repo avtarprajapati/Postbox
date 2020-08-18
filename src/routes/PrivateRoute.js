69842bc9222320edc9f226093b7483d358f43b7a
unknown <prajapatiavtar@gmail.com>
2020-08-18 16:10:05 +0530

    Check token proper

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,15 +2,12 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 function PrivateRoute({ component: Component, ...rest }) {
+  const isToken = localStorage.getItem("token");
   return (
     <Route
       {...rest}
       render={(props) =>
-        localStorage.getItem("token") ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
+        isToken ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
