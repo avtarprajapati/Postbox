051d7b43c35147d14ca8e3c6c25c991040cbe72a
unknown <prajapatiavtar@gmail.com>
2020-08-23 13:15:55 +0530

    add other User Profile View

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ import Login from "./Login";
 import Register from "./Register";
 import Profile from "./Profile";
 import Upload from "./Upload";
-import EditProfile from "./EditProfile";
+import ProfileView from "./EditProfile";
 
-export { Home, Explore, Login, Register, Profile, Upload, EditProfile };
+export { Home, Explore, Login, Register, Profile, Upload, ProfileView };
