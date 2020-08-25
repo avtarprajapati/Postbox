6061a84fd37e9aae145f4d781406e8191cae0591
karandas777 <karandas.21.08.98@gmail.com>
2020-08-25 15:53:44 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ export class Title extends Component {
     return (
       <div className="container-fluid py-0 px-0 bg-light shadow-sm">
         <div className="container px-0">
-          <nav className="navbar navbar-expand-lg navbar-light p-1">
+          <nav className="navbar navbar-expand-lg navbar-light p-2">
             <Link
               className="h3 mb-0 text-dark text-decoration-none logo"
               to="/"
@@ -41,59 +41,66 @@ export class Title extends Component {
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to="/">
-                  <i className="fa fa-home fa-fw fa-2x text-dark"></i> Home
+                    <i className="fa fa-home fa-fw fa-2x text-dark"></i>
+                    <span className="autohide"> Home</span>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="/explore">
-                  <i className="fa fa-compass fa-fw fa-2x text-dark"></i> Explore
+                    <i className="fa fa-compass fa-fw fa-2x text-dark"></i>
+                    <span className="autohide"> Explore</span>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="/upload">
-                  <i className="fa fa-plus-circle fa-fw fa-2x text-dark"></i> Upload
+                    <i className="fa fa-plus-circle fa-fw fa-2x text-dark"></i>
+                    <span className="autohide"> Upload</span>
                   </Link>
                 </li>
 
                 <li className="nav-item dropdown">
-                <button className="btn btn-light btn-lg pt-1 pl-1 pr-2 pb-0 rounded-pill dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
-                <img
-                      src={currentUser.imgurl}
-                      alt="profile-pic"
-                      className="rounded-pill bg-grad-1 p-1 mb-1 mr-2"
-                      style={{ width: "40px", height: "40px" }}
-                />
-                {currentUser.name}
-                </button>
-                
-                  <div
-                    className="dropdown-menu dropdown-menu-right shadow-sm border-0"
-                    aria-labelledby="navbarDropdownMenuLink"
-                  ><ul className="list-group list-group-flush">
-                    <li className="dropdown-item p-2 disabled text-center">
+                  <button
+                    className="btn bg-grad-1 rounded-pill btn-lg p-1 mb-1 ml-2"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                  >
                     <img
                       src={currentUser.imgurl}
                       alt="profile-pic"
-                      height="80"
-                      width="80"
-                      className="bg-grad-1 rounded-pill p-1 mb-3 mr-1"
-                    /><br/>
-                    {currentUser.name}
-                    </li>
+                      className="rounded-pill"
+                      height="40"
+                      width="40"
+                    />
+                  </button><span className="autohide"><i className="fa fa-caret-down mr-2"></i></span>
+                  
 
-                    <div className="dropdown-divider"></div>
-
-                    <li className="dropdown-item px-1">
-                      <Link className="nav-link" to="/profile">
-                        <i className="fa fa-user-circle mr-2"></i>Profile
-                      </Link>
-                    </li>
-                    <li className="dropdown-item px-1">
-                      <Link className="nav-link" to="/" onClick={this.onLogOut}>
-                        <i className="fa fa-power-off text-danger mr-2"></i>
-                        Logout
-                      </Link>
-                    </li>
+                  <div
+                    className="dropdown-menu dropdown-menu-right shadow-sm border-0"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <ul className="list-group list-group-flush">
+                      <li className="dropdown-item px-2 disabled">
+                        <div className="h5 mb-0">
+                          <i className="fa fa-user-circle mr-2"></i>
+                          {currentUser.name}
+                        </div>
+                      </li>
+                      <div className="dropdown-divider"></div>
+                      <li className="dropdown-item p-1">
+                        <Link className="nav-link" to="/profile">
+                          <i className="fa fa-id-card-o mr-2"></i>Profile
+                        </Link>
+                      </li>
+                      <li className="dropdown-item p-1">
+                        <Link
+                          className="nav-link"
+                          to="/"
+                          onClick={this.onLogOut}
+                        >
+                          <i className="fa fa-power-off text-danger mr-2"></i>
+                          Logout
+                        </Link>
+                      </li>
                     </ul>
                   </div>
                 </li>
